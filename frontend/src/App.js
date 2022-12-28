f60062ffdafccb8a7defa92c8bcfd137a016e2ce
Klemen <kklemen.stefe@gmail.com>
2022-12-28 21:11:08 +0100

    Added simple frontend to get Cars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
+
+const getCars = async () =>{
+  const response = await fetch('http://127.0.0.1:8000/cars/');
+  const data = await response.json();
+  return data
+};
 
 function App() {
+
+  const[cars, setCars] = useState([]);
+
+  const onClickHandler = async () => {
+    const data = await getCars();
+    setCars(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>List of cars</h1>
+        <button onClick={onClickHandler}>Click me to get cars!</button>
+      </div>
+      <div>
+        
+        {cars.length > 0 && (
+          <div>
+            {cars.map(car => (
+              <div key={car.id}>
+                <span>CAR: {car.id}</span>
+                <br></br>
+                <li>Car name: {car.name}</li>
+                <li>Car alias: {car.alias}</li>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
